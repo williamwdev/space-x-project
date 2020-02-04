61f6319871b1c44094877900a546bac987102877
William Y Wong <wwong1987@gmail.com>
2020-02-03 22:37:54 -0800

    defined schema types for Launch, Rocket, User, and Mission

--- final/server/src/schema.js
+++ final/server/src/schema.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const { gql } = require('apollo-server');
 
 const typeDefs = gql`
--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -1,10 +1,43 @@
-'use strict';
+"use strict";
 
-const { gql } = require('apollo-server');
+const { gql } = require("apollo-server");
+
+
+/* NOTE: 
+- An exclamation point (!) after a delcared field type means that the value can never be null
+- If a declared field's type is in [], its an array of the specified type && if an array has an exclamation point after it, the array cannot be null, but it CAN be empty
+*/
 
 const typeDefs = gql`
-  # Your schema will go here
+  type Launch {
+    id: ID!
+    site: String
+    mission: Mission
+    rocket: Rocket
+    isBooked: Boolean!
+  }
+
+  type Rocket {
+    id: ID!
+    name: String
+    type: String
+  }
+  
+  type User {
+    id: ID!
+    email: String!
+    trips: [Launch]!
+  }
+  
+  type Mission {
+    name: String
+    missionPatch(size: PatchSize): String
+  }
+
+  enum PatchSize {
+    SMALL
+    LARGE
+  }
 `;
 
 module.exports = typeDefs;
-
