0c49c4d0e4856aa4ccf94c02be2c224d9de3f6f1
William Y Wong <wwong1987@gmail.com>
2020-02-07 22:09:51 -0800

    added async getAllLaunches method to LaunchAPI class

--- start/server/src/datasources/launch.js
+++ start/server/src/datasources/launch.js
@@ -1,11 +1,19 @@
-'use strict';
+"use strict";
 
-const { RESTDataSource } = require('apollo-datasource-rest');
+const { RESTDataSource } = require("apollo-datasource-rest");
 
 class LaunchAPI extends RESTDataSource {
   constructor() {
     super();
-    this.baseURL = 'https://api.spacexdata.com/v2/';
+    this.baseURL = "https://api.spacexdata.com/v2/";
+  }
+
+  // method that maps over the launches and transforms the response from our REST endpoint with this.launchReducer
+  async getAllLaunches() {
+    const response = await this.get("launches"); // makes GET request to https://api.spacexdata.com/v2/launches & stores the returned launches in the response variable
+    return Array.isArray(response)
+      ? response.map(launch => this.launchReducer(launch)) 
+      : []; // if there are no responses then an empty array is returned
   }
 }
 
