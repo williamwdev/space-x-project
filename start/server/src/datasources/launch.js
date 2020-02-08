aca451fa81e6c4c6da91665dd440d0112fad29c9
William Y Wong <wwong1987@gmail.com>
2020-02-07 22:04:06 -0800

    installed apollo-datasource-rest and created LaunchAPI data source

---
+++ start/server/src/datasources/launch.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const { RESTDataSource } = require('apollo-datasource-rest');
+
+class LaunchAPI extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = 'https://api.spacexdata.com/v2/';
+  }
+}
+
+module.exports = LaunchAPI;
