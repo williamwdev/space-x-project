429365aa4645ad21a383cd09be2a430eef6d97fd
William Y Wong <wwong1987@gmail.com>
2020-02-10 20:52:13 -0800

    updated schema and added LaunchConnection

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -2,7 +2,6 @@
 
 const { gql } = require("apollo-server");
 
-
 /* NOTE: 
 - An exclamation point (!) after a delcared field type means that the value can never be null
 - If a declared field's type is in [], its an array of the specified type && if an array has an exclamation point after it, the array cannot be null, but it CAN be empty
@@ -24,13 +23,13 @@ const typeDefs = gql`
     name: String
     type: String
   }
-  
+
   type User {
     id: ID!
     email: String!
     trips: [Launch]!
   }
-  
+
   type Mission {
     name: String
     missionPatch(size: PatchSize): String
@@ -44,9 +43,29 @@ const typeDefs = gql`
   # ===== Query type for clients to execute against the data graph =====
 
   type Query {
-    launches: [Launch]!
+    launches( # replace the current launches query with this one.
+      """
+      The number of results to show. Must be >= 1. Default = 20
+      """
+      pageSize: Int
+      """
+      If you add a cursor here, it will only return results _after_ this cursor
+      """
+      after: String
+    ): LaunchConnection!
     launch(id: ID!): Launch
     me: User
+  }
+
+  """
+  Simple wrapper around our list of launches that contains a cursor to the
+  last item in the list. Pass this cursor to the launches query to fetch results
+  after these.
+  """
+  type LaunchConnection { # add this below the Query type as an additional type.
+    cursor: String!
+    hasMore: Boolean!
+    launches: [Launch]!
   }
 
   # ===== Mutation type that enables clients to modify data =====
@@ -56,13 +75,12 @@ const typeDefs = gql`
     cancelTrip(launchId: ID!): TripUpdateResponse!
     login(email: String): String # login token
   }
-  
+
   type TripUpdateResponse {
     success: Boolean!
     message: String
     launches: [Launch]
   }
-
 `;
 
 module.exports = typeDefs;
