79341bf082741648c8a8e5f6dfb9959118225661
William Y Wong <wwong1987@gmail.com>
2020-02-10 20:39:58 -0800

    added query to resolvers

---
+++ start/server/src/resolvers.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  Query: {
+    launches: (_, __, { dataSources }) =>
+      dataSources.launchAPI.getAllLaunches(),
+    launch: (_, { id }, { dataSources }) =>
+      dataSources.launchAPI.getLaunchById({ launchId: id }),
+    me: (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser()
+  }
+};
